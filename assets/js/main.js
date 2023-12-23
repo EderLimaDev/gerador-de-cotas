39a1998b830212b6f9e84f50f596ac888bbf5ff3
EderLimaDev <ederlimarh@gmail.com>
2023-12-23 18:45:15 -0300

    implementando validações e ajuste de valores

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,16 @@ let cota = [];
 btn.addEventListener("click", () => {
   if (
     quantidadeDeCotasSolicitadas.value === "" ||
-    numerosPorCotas.value === ""
+    numerosPorCotas.value === "" ||
+    quantidadeDeCotasSolicitadas.value <= 0 ||
+    numerosPorCotas.value <= 0
   ) {
-    alert("Campo não pode ser vazio");
+    alert("Digite um número válido em ambos os campos");
   } else {
     for (let j = 0; j <= quantidadeDeCotasSolicitadas.value - 1; j++) {
       for (let i = 0; i < numerosPorCotas.value; i++) {
-        let numero = Math.round(Math.random() * 60);
-        cota.push(numero);
+        let numero = Math.round(Math.random() * 60) + 1;
+        cota.push(numero.toString().padStart(2, "0"));
       }
       const newLine = document.createElement("li");
       listaCotas.appendChild(newLine);
